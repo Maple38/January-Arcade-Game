7dda38b62828656a9e9e57c0c05f72cd2e5f8e37
@Maple38 <dimness_pampas_8s@icloud.com>
2026-01-02 20:17:40 +0300

    Cleaned up movement script, removing unnecesary bits, and further worked on it.
    The long break helped quite a bit, refreshed my perspective.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,10 @@ using UnityEngine.Serialization;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float impulseSpeed;
-    [FormerlySerializedAs("turnStep")] [SerializeField] private float angleIncrement;
+
+    [FormerlySerializedAs("turnStep")] [SerializeField]
+    private float angleIncrement;
+
     [SerializeField] private float forwardImpulseMult;
     [SerializeField] private int maxAngleIndex;
     [SerializeField] private float angleCorrectionAmount;
@@ -24,7 +27,7 @@ public class PlayerMovement : MonoBehaviour
     private InputAction _forwardAction;
     private InputAction _backAction;
     private InputAction _modAction;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -40,59 +43,50 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    // void Update()
-    // {
-    //     // A and D
-    //     if (_hardLeftAction.WasPressedThisFrame())
-    //     {
-    //         if (_modAction.IsPressed())
-    //         {
-    //             Impulse(90);
-    //         }
-    //         else
-    //         { 
-    //             _angleIndex = Mathf.Round(_angleIndex + 1);
-    //         }
-    //     }
-    //     else if (_hardRightAction.WasPressedThisFrame())
-    //     {
-    //         if (_modAction.IsPressed())
-    //         {
-    //             Impulse(-90);
-    //         }
-    //         else 
-    //         {
-    //             _angleIndex = Mathf.Round(_angleIndex - 1);
-    //         }
-    //     }
-    //
-    //     // W and S
-    //     if (_forwardAction.WasPressedThisFrame())
-    //     {
-    //         Impulse(0);
-    //     }
-    //     else if (_backAction.WasPressedThisFrame())
-    //     {
-    //         _angleIndex = 0;
-    //     }
-    //     
-    //     _angleIndex = Mathf.Clamp(_angleIndex, -maxAngleIndex, maxAngleIndex);
-    //     transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, _angleIndex * angleIncrement);
-    // }
-
     void Update()
     {
-        if (_hardLeftAction.IsPressed())
+        // A and D
+        if (_hardLeftAction.WasPressedThisFrame())
         {
-            _targetPos.localPosition += Vector2.left * Time.deltaTime * moveSpeed;
+            Impulse(90);
         }
+        else if (_hardRightAction.WasPressedThisFrame())
+        {
+            Impulse(-90);
+        }
+
+        // Q and E
+        if (_softLeftAction.IsPressed())
+        {
+            _angleIndex = Mathf.Round(_angleIndex + 1);
+        }
+        else if (_softRightAction.IsPressed())
+        {
+            _angleIndex = Mathf.Round(_angleIndex - 1);
+        }
+        else
+        {
+            _angleIndex = 0;
+        }
+
+        // W and S
+        if (_forwardAction.WasPressedThisFrame())
+        {
+            Impulse(0);
+        }
+        else if (_backAction.WasPressedThisFrame())
+        {
+            Impulse(180);
+        }
+
+        _angleIndex = Mathf.Clamp(_angleIndex, -maxAngleIndex, maxAngleIndex);
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, _angleIndex * angleIncrement);
     }
-    
+
     void Impulse(float impulseAngle)
     {
         float internalImpulseSpeed;
-        float correctionMult = 0;
-        
+
         if (impulseAngle == 0)
         {
             internalImpulseSpeed = impulseSpeed * forwardImpulseMult;
@@ -101,39 +95,12 @@ public class PlayerMovement : MonoBehaviour
         {
             internalImpulseSpeed = impulseSpeed;
         }
-        
-        // Intuitively you'd assume that if the impulse and the rotation are in the same direction, they'd have the
-        // same sign, but that's not the case as one operates clockwise and the other counterclockwise.
-        // ReSharper disable once CompareOfFloatsByEqualityOperator
-        if (Mathf.Sign(impulseAngle) == Mathf.Sign(_angleIndex))
-        {
-            // We're dashing in the same direction we're rotated, so don't correct as much.
-            correctionMult = 1;
-        }
-        else
-        {
-            // If we're dashing in the opposite direction, correct twice as much, this feels better.
-            correctionMult = 2;
-        }
-        
-        if (_angleIndex > 0 + angleCorrectionAmount)
-        {
-            _angleIndex -= angleCorrectionAmount * correctionMult;
-        }
-        else if (_angleIndex < 0 - angleCorrectionAmount)
-        {
-            _angleIndex += angleCorrectionAmount * correctionMult;
-        }
-        else
-        {
-            _angleIndex = 0;
-        }
 
         Vector2 force = new Vector2(
             -Mathf.Sin(Mathf.Deg2Rad * (impulseAngle + transform.eulerAngles.z)) * internalImpulseSpeed,
             Mathf.Cos(Mathf.Deg2Rad * (impulseAngle + transform.eulerAngles.z)) * internalImpulseSpeed);
-        print(force);
-        _rb.AddForce(force, 
+
+        _rb.AddForce(force,
             ForceMode2D.Impulse);
     }
 }
