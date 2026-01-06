1664fe536e52019b2a5da432bc92d0b8f5846c6f
@Maple38 <dimness_pampas_8s@icloud.com>
2026-01-07 02:05:01 +0300

    Added a ramming attack

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,24 +1,58 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerAttack : MonoBehaviour
 {
+    private Rigidbody2D _rb;
+
+    [Header("Targeting Settings")] 
     [SerializeField] private float targetingRange;
     [SerializeField] private LayerMask layerMask;
     [SerializeField] private float scanRange;
     [SerializeField] private GameObject crosshairObject;
-    [SerializeField] private float ramRange;
+    [SerializeField] private float ramSpeedMult;
+    
+    private float _targetingGracePeriod;
     private Vector2 _targetPos;
     private SpriteRenderer _crosshairSr;
+    private Boolean _targetReady;
 
-    void Awake()
+    [Header("Ramming")] [SerializeField] private float ramChargeMin;
+    [SerializeField] private float ramChargeMax;
+    [SerializeField] private float ramPowerMin;
+    [SerializeField] private float ramPowerMax;
+    public float ramChargeMultDefault;
+    
+    public float RamPowerCurrent { get; private set; }
+    public Boolean RamInProgress { get; private set; }
+    [DoNotSerialize] public float ramChargeMult;
+    private float _ramCooldownCurrent;
+    private float _ramCooldownMax;
+    private float _ramCharge;
+
+    private void Awake()
     {
+        _rb = GetComponent<Rigidbody2D>();
         _crosshairSr = crosshairObject.GetComponent<SpriteRenderer>();
     }
 
-    void Update()
+    private void Update()
     {
         TargetScan();
+        if (_ramCooldownCurrent >= 0)
+        {
+            _ramCooldownCurrent -= Time.deltaTime;
+        }
+
+        if (_ramCharge >= 0)
+        {
+            // Tick down the ram charge at all times. Charging is multiplied by 2 to account for this.
+            _ramCharge -= Time.deltaTime;
+        }
     }
 
     private void TargetScan()
@@ -33,10 +67,14 @@ public class PlayerAttack : MonoBehaviour
             _targetPos = scanResult.collider.bounds.center;
             crosshairObject.transform.position = _targetPos;
             _crosshairSr.enabled = true;
+            _targetReady = true;
+            _targetingGracePeriod = 0.2f;
         }
         else
         {
             _crosshairSr.enabled = false;
+            _targetReady = false;
+            _targetingGracePeriod -= Time.deltaTime;
         }
     }
 
@@ -51,5 +89,59 @@ public class PlayerAttack : MonoBehaviour
         // Gizmos.DrawRay(transform.position,
         //     new Vector2(Mathf.Cos((transform.rotation.eulerAngles.z + 90) * Mathf.Deg2Rad),
         //         Mathf.Sin((transform.rotation.eulerAngles.z + 90) * Mathf.Deg2Rad)) * 4);
+    }
+
+    public void ChargeRam()
+    {
+        if (_ramCooldownCurrent <= 0)
+        {
+            // Charging speed is multiplied by 2 to account for the decay
+            _ramCharge += Time.deltaTime * 2 * ramChargeMult;
+        }
+
+        if (_ramCharge >= ramChargeMax)
+        {
+            // TODO update animation
+        }
+        else if (_ramCharge >= ramChargeMin)
+        {
+            // TODO update animation
+        }
+    }
+
+    public void RamRelease()
+    {
+        if (_targetReady || _targetingGracePeriod >= 0)
+        {
+            if (_ramCharge >= ramChargeMin)
+            {
+                if (_ramCharge >= ramChargeMax)
+                {
+                    RamPowerCurrent = ramPowerMax;
+                }
+                else
+                {
+                    RamPowerCurrent = ramPowerMin;
+                }
+
+                RamInProgress = true;
+                _ramCooldownCurrent = _ramCooldownMax;
+                StartCoroutine(RamCoroutine(transform.position, _targetPos));
+            }
+        }
+    }
+
+    private IEnumerator RamCoroutine(Vector2 start, Vector2 end)
+    {
+        float moveProgress = 0;
+        while (moveProgress <= 1f)
+        {
+            float moveDelta = Time.deltaTime * ramSpeedMult;
+            moveProgress += moveDelta;
+            transform.position = Vector2.LerpUnclamped(start, end, moveProgress);
+            yield return new WaitForFixedUpdate();
+        }
+
+        RamInProgress = false;
     }
 }
