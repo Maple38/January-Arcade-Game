d6e72d7dccb0e77538689c17633a737d30b821e6
@Maple38 <dimness_pampas_8s@icloud.com>
2026-01-07 22:04:01 +0300

    Created GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int scoreMult = 10;
+    public static GameManager Instance { get; private set; }
+    public int Score { get; private set; }
+    public int Difficulty { get; private set; } = 1;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void AddScore(int amount)
+    {
+        Score += scoreMult * amount;
+    }
+}
