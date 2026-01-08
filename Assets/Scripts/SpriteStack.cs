f4384387df74aa944dc71cbc394ccbc476288e2b
@Maple38 <dimness_pampas_8s@icloud.com>
2026-01-09 02:22:19 +0300

    Changed the sprite stacking script to have just one multiplier per layer rather than having it axis dependant

--- Assets/Scripts/SpriteStack.cs
+++ Assets/Scripts/SpriteStack.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 public class SpriteStack : MonoBehaviour
 {
     [SerializeField] private GameObject[] layers;
-    [SerializeField] private Vector2[] multipliers;
+    [SerializeField] private float[] multipliers;
     [SerializeField] private float vMult;
     [SerializeField] private float hMult;
 
     private int _layerCount;
-    private Vector2[] _individualMults;
+    private float[] _individualMults;
     private Transform[] _transforms;
 
     void Awake()
     {
         _layerCount = layers.Length;
         _transforms = new Transform[_layerCount];
-        _individualMults = new Vector2[_layerCount];
+        _individualMults = new float[_layerCount];
 
         for (int i = 0; i < _layerCount; i++)
         {
@@ -38,9 +38,9 @@ public class SpriteStack : MonoBehaviour
         }
     }
 
-    void ComputeParallax(Transform layerTransform, Vector2 currentPos, Vector2 mults)
+    void ComputeParallax(Transform layerTransform, Vector2 currentPos, float mult)
     {
         layerTransform.localPosition =
-            new Vector2(currentPos.x * hMult * 0.1f, currentPos.y * vMult * 0.1f) * mults;
+            new Vector2(currentPos.x * hMult * 0.1f, currentPos.y * vMult * 0.1f) * mult;
     }
 }
