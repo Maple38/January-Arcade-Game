8bb5dfd356217a5b721311be3129b3210ac989b4
@Maple38 <dimness_pampas_8s@icloud.com>
2026-01-15 01:58:37 +0300

    Implemented basic code allowing the vehicle to drive at a configured throttle and automatically determined direction

--- Assets/Scripts/Enemy/VehicleBrainDirect.cs
+++ Assets/Scripts/Enemy/VehicleBrainDirect.cs
@@ -1,21 +1,33 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class VehicleBrainDirect : MonoBehaviour
 {
     private Vector2 _targetPos;
+    private VehicleController _controller;
+    [SerializeField] private float throttleOverTen;
     
     void Start()
     {
+        _controller = GetComponent<VehicleController>();
         _targetPos = new Vector2(0, 0);
     }
     
     void Update()
     {
+        _controller.Steer(CalculateSteering());
+        _controller.Throttle(throttleOverTen/10);
         if (Input.GetMouseButtonDown(0))
         {
             _targetPos = (Camera.main.ScreenToWorldPoint(Input.mousePosition));
         }
+    }
+
+    private float CalculateSteering()
+    {
+        var targetDir = _targetPos - (Vector2)transform.position;
+        return Vector2.SignedAngle(transform.up, targetDir);
     }
 
     private void OnDrawGizmosSelected()
