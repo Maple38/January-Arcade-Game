5af63270274f46e301610577c902d9135698db52
@Maple38 <dimness_pampas_8s@icloud.com>
2026-01-19 12:39:00 +0000

    Wrote a script for spawning enemies

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Bounds bounds;
+    [SerializeField] private float spawnDelay;
+    [SerializeField] private GameObject[] enemyPrefabs;
+    private int _prefabCount;
+
+    private void Start()
+    {
+        _prefabCount = enemyPrefabs.Length;
+        StartCoroutine(SpawnRoutine());
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            Instantiate(enemyPrefabs[Random.Range(0, _prefabCount - 1)],
+                new Vector3(Random.Range(bounds.min.x, bounds.max.x), Random.Range(bounds.min.y, bounds.max.y), 0),
+                Quaternion.identity);
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(bounds.center, bounds.size);
+    }
+}
