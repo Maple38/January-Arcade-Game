9fc518d35ef33af2aa75cec9506106cbe717d916
@Maple38 <dimness_pampas_8s@icloud.com>
2026-01-19 02:06:05 +0000

    Wrote code to sync the heart animations

--- Assets/Scripts/HealthbarController.cs
+++ Assets/Scripts/HealthbarController.cs
@@ -1,13 +1,11 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class HealthbarController : MonoBehaviour
 {
     private HeartController[] _hearts;
     [SerializeField] private int testHealthValue; // For debugging purposes
 
-    // For debugging purposes
+    // For debugging purposes, set testHealthValue in the inspector, right-click the component, and choose this to run the function
     [ContextMenu("Apply Test Value")] 
     void TestHealth()
     {
@@ -26,18 +24,30 @@ public class HealthbarController : MonoBehaviour
         {
             switch (health)
             {
+                // If there's 2 or more health remaining, show this heart as full and get rid of 2hp from the pool
                 case >= 2:
                     heart.SetState(2);
                     health -= 2;
                     break;
+                // If there's only 1hp left in the pool, then we need this heart to be a half heart, and drain the pool
                 case 1:
                     heart.SetState(1);
                     health -= 1;
                     break;
+                // Health pool is empty, nothing left to give to this heart, so make it empty
                 case >= 0:
                     heart.SetState(0);
                     break;
             }
+        }
+        
+        // Use the first heart in the array to determine the animation progress to be used for the rest
+        var animTime = _hearts[0].GetTime();
+        // Loop through the remaining hearts, overriding their animation progress to be in sync.
+        // When the animation switches, it also restarts, leading to the hearts not being in sync. This fixes that.
+        for (int i = 1; i < _hearts.Length; i++)
+        {
+            _hearts[i].SyncTime(animTime);
         }
     }
 }
--- Assets/Scripts/HeartController.cs
+++ Assets/Scripts/HeartController.cs
@@ -4,15 +4,33 @@ public class HeartController : MonoBehaviour
 {
     private Animator _animator;
     private int _propertyHash;
+    
     void Start()
     {
         _animator = GetComponent<Animator>();
-        _propertyHash = Animator.StringToHash("State");
+        _propertyHash = Animator.StringToHash("State"); // Hash this for efficiency
     }
 
+    // Updates the "state" property to an integer, determining which sprite should show
     public void SetState(int state)
     {
         // 0 = Empty, 1 = Half, 2 = Full
         _animator.SetInteger(_propertyHash, state);
+    }
+
+    // Overrides the current playback timestamp. This is because after changing the animation, it'll restart,
+    // making the glint effect not be in sync. So we use this function to sync all the hearts.
+    public void SyncTime(float time)
+    {
+        AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+        _animator.Play(stateInfo.fullPathHash, 0, time);
+    }
+    
+    // Gets the current progress into playing the animation clip
+    public float GetTime()
+    {
+        // Fetch the current animation status
+        AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+        return stateInfo.normalizedTime;
     }
 }
